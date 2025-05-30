ab2c956b3780832f0cfe9453842f7f5a698d19a3
k33m0l <k33m0l@users.noreply.github.com>
2025-05-30 22:08:32 +0200

    wallpaper

--- fetcher.sh
+++ fetcher.sh
@@ -4,3 +4,5 @@ echo 'Fetch Hyprland dotfiles'
 cp $HOME/.config/hypr/hyprland.conf ./hyprland/hyprland.conf
 cp $HOME/.config/hypr/hyprpaper.conf ./hyprland/hyprpaper.conf
 
+echo 'Fetch Wallpapers'
+cp $HOME/Pictures/Wallpapers/rei_blue.png ./wallpaper/rei_blue.png
