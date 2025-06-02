6fa66d345ee37b0643d142ccde89f060f5946568
k33m0l <k33m0l@users.noreply.github.com>
2025-06-02 19:37:52 +0200

    nvim added

--- fetcher.sh
+++ fetcher.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 echo 'Fetch Hyprland dotfiles'
-cp $HOME/.config/hypr/hyprland.conf ./hyprland/hyprland.conf
-cp $HOME/.config/hypr/hyprpaper.conf ./hyprland/hyprpaper.conf
+cp -v $HOME/.config/hypr/hyprland.conf ./hyprland/hyprland.conf
+cp -v $HOME/.config/hypr/hyprpaper.conf ./hyprland/hyprpaper.conf
 
 echo 'Fetch Wallpapers'
-cp $HOME/Pictures/Wallpapers/*.png ./wallpaper/
+cp -v $HOME/Pictures/Wallpapers/*.png ./wallpaper/
 
 echo 'Fetch Waybar'
-cp -r $HOME/.config/waybar/* ./waybar/
+cp -vr $HOME/.config/waybar/* ./waybar/
 
+echo 'Fetch NeoVim Configuration'
+cp -v $HOME/.config/nvim/init.lua ./nvim/init.lua
+cp -v $HOME/.config/nvim/.stylua.toml ./nvim/.stylua.toml
+cp -v $HOME/.config/nvim/lazy-lock.json ./nvim/lazy-lock.json
+cp -vr $HOME/.config/nvim/lua/* ./nvim/lua/
